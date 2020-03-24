ae78c4b4ee2aa5e6a6ad51dbda5ee12414164749
Daniel Sokil <dtechtours@gmail.com>
2020-03-23 21:08:52 -0400

    Another Example

--- examples/lit-html/index.js
+++ examples/lit-html/index.js
@@ -6,15 +6,17 @@ _.person = {
   age: Infinity
 };
 
+const personNode = document.body.appendChild(document.createElement("div"));
 $$(person => {
   const Person = person =>
     html`
       <div>${person.name} is ${person.age} years old.</div>
     `;
 
-  render(Person(person), document.body);
+  render(Person(person), personNode);
 });
 
+/*
 setInterval(() => {
   $({
     person: {
@@ -22,4 +24,21 @@ setInterval(() => {
       age: Math.round(Math.random() * 10000)
     }
   });
-}, 0 /* quick! */);
+}, 1000);
+*/
+
+_.counter = 0;
+_.CounterComponent = $$(counter => {
+  return html`
+    <button @click=${() => $({ counter: _.counter - 1 })}>-</button>
+    <span>${String(counter)}</span>
+    <button @click=${() => $({ counter: _.counter + 1 })}>+</button>
+  `;
+});
+
+function main() {
+  const rootNode = document.body.appendChild(document.createElement("div"));
+
+  render(_.CounterComponent, rootNode);
+}
+main();
